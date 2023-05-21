6661a3952b7b93be41fd4bf7760bc6861da3b710
Paulo Santana <paulo.santana.r@gmail.com>
2023-05-21 17:26:26 -0300

    feat: return a sky color when missing rays

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 use std::io;
 
-use raytracing::color::{self, Color};
+use raytracing::{
+    color::{self, Color},
+    ray::Ray,
+    vec3::Point3,
+    vec3::Vec3,
+};
 
-pub fn ratio(a: u32, b: u32) -> f64 {
+fn ratio(a: u32, b: u32) -> f64 {
     a as f64 / (b as f64 - 1.0)
 }
+
+fn ray_color(ray: Ray) -> Color {
+    let unit_direction = Color::unit_vector(&ray.direction);
+    let t = 0.5 * (unit_direction.y() + 1.0);
+
+    return (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0);
+}
+
 fn main() {
     // image
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+    // camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        &origin - &horizontal / 2.0 - &vertical / 2.0 - Vec3(0.0, 0.0, focal_length);
 
     // render
     println!("P3");
@@ -18,7 +43,13 @@ fn main() {
     for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaininig: {j} ");
         for i in 0..IMAGE_WIDTH {
-            let color = Color::new(ratio(i, IMAGE_WIDTH), ratio(j, IMAGE_HEIGHT), 0.25);
+            let u = ratio(i, IMAGE_WIDTH);
+            let v = ratio(j, IMAGE_HEIGHT);
+            let ray = Ray::new(
+                origin,
+                &lower_left_corner + u * &horizontal + v * &vertical - &origin,
+            );
+            let color = ray_color(ray);
             color::write_color(&mut io::stdout(), &color);
         }
     }
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,12 +1,12 @@
 use std::ops;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Vec3(pub f64, pub f64, pub f64);
 pub use Vec3 as Point3;
 
 impl Vec3 {
-    pub fn unit_vector(v: Vec3) -> Vec3 {
-        &v / v.length()
+    pub fn unit_vector(v: &Vec3) -> Vec3 {
+        v / v.length()
     }
     pub fn new(r: f64, g: f64, b: f64) -> Vec3 {
         Vec3(r, g, b)
@@ -82,6 +82,22 @@ impl ops::AddAssign<&Vec3> for Vec3 {
     }
 }
 
+impl ops::Sub<Vec3> for &Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Self::Output {
+        Vec3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Self::Output {
+        Vec3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
 impl ops::Sub<&Vec3> for Vec3 {
     type Output = Vec3;
 
@@ -106,11 +122,19 @@ impl ops::Mul<f64> for Vec3 {
     }
 }
 
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, vec: Vec3) -> Self::Output {
+        Vec3(self * vec.0, self * vec.1, self * vec.2)
+    }
+}
+
 impl ops::Mul<&Vec3> for f64 {
     type Output = Vec3;
 
-    fn mul(self, other: &Vec3) -> Self::Output {
-        Vec3(self * other.0, self * other.1, self * other.2)
+    fn mul(self, vec: &Vec3) -> Self::Output {
+        Vec3(self * vec.0, self * vec.1, self * vec.2)
     }
 }
 
@@ -123,6 +147,14 @@ impl ops::MulAssign<f64> for Vec3 {
 }
 
 impl ops::Div<f64> for &Vec3 {
+    type Output = Vec3;
+
+    fn div(self, value: f64) -> Self::Output {
+        Vec3(self.0 / value, self.1 / value, self.2 / value)
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
     type Output = Vec3;
 
     fn div(self, value: f64) -> Self::Output {
