63c7e52c3f32db0803bc10881297ae4d8cbf8721
Paulo Santana <paulo.santana.r@gmail.com>
2023-05-21 15:27:58 -0300

    feat: add more utility functions for Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -16,6 +16,10 @@ impl Vec3 {
     pub fn z(&self) -> f64 {
         self.2
     }
+
+    pub fn dot(&self, other: &Vec3) -> f64 {
+        (self.0 * other.0) + (self.1 * other.1) + (self.2 * other.2)
+    }
 }
 
 impl ops::Neg for &Vec3 {
@@ -29,31 +33,71 @@ impl ops::Neg for &Vec3 {
 impl ops::Add<&Vec3> for Vec3 {
     type Output = Vec3;
 
-    fn add(self, rhs: &Vec3) -> Self::Output {
-        Vec3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
+    fn add(self, other: &Vec3) -> Self::Output {
+        Vec3(self.0 + other.0, self.1 + other.1, self.2 + other.2)
     }
 }
 
 impl ops::AddAssign<&Vec3> for Vec3 {
-    fn add_assign(&mut self, rhs: &Vec3) {
-        self.0 += rhs.0;
-        self.1 += rhs.1;
-        self.2 += rhs.2;
+    fn add_assign(&mut self, other: &Vec3) {
+        self.0 += other.0;
+        self.1 += other.1;
+        self.2 += other.2;
+    }
+}
+
+impl ops::Sub<&Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: &Vec3) -> Self::Output {
+        Vec3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+}
+
+impl ops::Mul<&Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: &Vec3) -> Self::Output {
+        Vec3(self.0 * other.0, self.1 * other.1, self.2 * other.2)
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, value: f64) -> Self::Output {
+        Vec3(self.0 * value, self.1 * value, self.2 * value)
+    }
+}
+
+impl ops::Mul<&Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, other: &Vec3) -> Self::Output {
+        Vec3(self * other.0, self * other.1, self * other.2)
     }
 }
 
 impl ops::MulAssign<f64> for Vec3 {
-    fn mul_assign(&mut self, rhs: f64) {
-        self.0 *= rhs;
-        self.1 *= rhs;
-        self.2 *= rhs;
+    fn mul_assign(&mut self, value: f64) {
+        self.0 *= value;
+        self.1 *= value;
+        self.2 *= value;
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, value: f64) -> Self::Output {
+        Vec3(self.0 / value, self.1 / value, self.2 / value)
     }
 }
 
 impl ops::DivAssign<f64> for Vec3 {
-    fn div_assign(&mut self, rhs: f64) {
-        self.0 /= rhs;
-        self.1 /= rhs;
-        self.2 /= rhs;
+    fn div_assign(&mut self, value: f64) {
+        self.0 /= value;
+        self.1 /= value;
+        self.2 /= value;
     }
 }
