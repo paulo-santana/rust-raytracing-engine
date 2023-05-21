8da80009a1011a4f629f631a51948f6fce2cd525
Paulo Santana <paulo.santana.r@gmail.com>
2023-05-21 15:06:45 -0300

    refactor: change methods orders

--- src/vec3.rs
+++ src/vec3.rs
@@ -18,19 +18,19 @@ impl Vec3 {
     }
 }
 
-impl ops::Add<&Vec3> for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, rhs: &Vec3) -> Self::Output {
-        Vec3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
-    }
-}
-
 impl ops::Neg for &Vec3 {
     type Output = Vec3;
 
     fn neg(self) -> Self::Output {
         Vec3(-self.0, -self.1, -self.2)
+    }
+}
+
+impl ops::Add<&Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: &Vec3) -> Self::Output {
+        Vec3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
     }
 }
 
