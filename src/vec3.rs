e1c855616717fb15682d8d7db6440ba251d4d2e1
Paulo Santana <paulo.santana.r@gmail.com>
2023-05-21 14:58:59 -0300

    fix: pass self by reference on utility methods

--- src/vec3.rs
+++ src/vec3.rs
@@ -7,26 +7,26 @@ impl Vec3 {
     pub fn new(r: f64, g: f64, b: f64) -> Vec3 {
         Vec3(r, g, b)
     }
-    pub fn x(self) -> f64 {
+    pub fn x(&self) -> f64 {
         self.0
     }
-    pub fn y(self) -> f64 {
+    pub fn y(&self) -> f64 {
         self.1
     }
-    pub fn z(self) -> f64 {
+    pub fn z(&self) -> f64 {
         self.2
     }
 }
 
-impl ops::Add<Vec3> for Vec3 {
+impl ops::Add<&Vec3> for Vec3 {
     type Output = Vec3;
 
-    fn add(self, rhs: Vec3) -> Self::Output {
+    fn add(self, rhs: &Vec3) -> Self::Output {
         Vec3(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
     }
 }
 
-impl ops::Neg for Vec3 {
+impl ops::Neg for &Vec3 {
     type Output = Vec3;
 
     fn neg(self) -> Self::Output {
@@ -34,8 +34,8 @@ impl ops::Neg for Vec3 {
     }
 }
 
-impl ops::AddAssign for Vec3 {
-    fn add_assign(&mut self, rhs: Vec3) {
+impl ops::AddAssign<&Vec3> for Vec3 {
+    fn add_assign(&mut self, rhs: &Vec3) {
         self.0 += rhs.0;
         self.1 += rhs.1;
         self.2 += rhs.2;
