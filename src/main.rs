134c6dc9c8fab96275996345136d081b6b60d6e0
Paulo Santana <paulo.santana.r@gmail.com>
2023-05-21 18:01:51 -0300

    feat: draw a sphere based on its normals

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,26 @@ fn ratio(a: u32, b: u32) -> f64 {
     a as f64 / (b as f64 - 1.0)
 }
 
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
+    let oc = ray.origin - center;
+    let a = ray.direction.dot(&ray.direction);
+    let b = 2.0 * oc.dot(&ray.direction);
+    let c = oc.dot(&oc) - radius * radius;
+
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
+}
+
 fn ray_color(ray: Ray) -> Color {
+    let t = hit_sphere(&Point3(0.0, 0.0, -1.0), 0.5, &ray);
+    if t > 0.0 {
+        let n = Vec3::unit_vector(&(ray.at(t) - Vec3(0.0, 0.0, -1.0)));
+        return 0.5 * Color(n.0 + 1.0, n.1 + 1.0, n.2 + 1.0);
+    }
     let unit_direction = Color::unit_vector(&ray.direction);
     let t = 0.5 * (unit_direction.y() + 1.0);
 
