84c93f51f60ac8bd4a89d995443e692c00925a71
Paulo Santana <paulo.santana.r@gmail.com>
2023-05-21 16:46:11 -0300

    feat: add more linalg functions to Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -5,6 +5,9 @@ pub struct Vec3(pub f64, pub f64, pub f64);
 pub use Vec3 as Point3;
 
 impl Vec3 {
+    pub fn unit_vector(v: Vec3) -> Vec3 {
+        &v / v.length()
+    }
     pub fn new(r: f64, g: f64, b: f64) -> Vec3 {
         Vec3(r, g, b)
     }
@@ -18,8 +21,24 @@ impl Vec3 {
         self.2
     }
 
+    pub fn length(&self) -> f64 {
+        self.lenght_squared().sqrt()
+    }
+
+    pub fn lenght_squared(&self) -> f64 {
+        self.dot(self)
+    }
+
     pub fn dot(&self, other: &Vec3) -> f64 {
         (self.0 * other.0) + (self.1 * other.1) + (self.2 * other.2)
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3(
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.0 * other.1 - self.1 * other.0,
+        )
     }
 }
 
@@ -103,7 +122,7 @@ impl ops::MulAssign<f64> for Vec3 {
     }
 }
 
-impl ops::Div<f64> for Vec3 {
+impl ops::Div<f64> for &Vec3 {
     type Output = Vec3;
 
     fn div(self, value: f64) -> Self::Output {
